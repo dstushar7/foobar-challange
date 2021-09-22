d1c0c088e7cfbd0d705da6867f0196179a3e385a
dstushar7 <tusharsarker97@gmail.com>
2021-09-22 22:30:19 +0600

    Challenge 2 Loading

--- Challenge2/solution.py
+++ Challenge2/solution.py
@@ -2,3 +2,12 @@
 #     print(5)
 
 list = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
+
+
+
+#Breaking all strings into list
+for i in range(0,len(list)):
+    list[i] = list[i].split(".")
+    
+
+print(list)
